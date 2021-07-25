047b827597c0f69687960da117078c286942d060
JenniferSoares <80266450+JenniferSoares@users.noreply.github.com>
2021-07-25 20:21:53 -0300

    alteracao resposta put

--- api/views.py
+++ api/views.py
@@ -99,7 +99,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
                     PackageRelease.objects.create(name=package['name'], version=package['version'], project=obj)
 
                 serializer = ProjectSerializer(obj)
-                return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.data, status=status.HTTP_200_OK)
 
             else:
                 return Response({"error":"One or more packages doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
